f1a2eb56bc951ad9eac3fe0ea761113a089db23d
Robin Painter <lochlanpainter@gmail.com>
2025-01-04 23:04:01 +1100

    Updated to Bit 2025.1.0

--- example-plugin/index.js
+++ example-plugin/index.js
@@ -1,5 +1,8 @@
+const core = require("bit/core");
+
 module.exports = {
     startFunction: function startFunction() {
-        console.log("Example Plugin Successfully Loaded!")
+        // Sends a message to the Bit Core plugin and asks it to run the logging function!
+        core.log(0, "Example Plugin", true, "Example Plugin for example stuffs")
     }
 };
