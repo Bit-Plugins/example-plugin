37f553000aab6ce1ab04f0867f4710639fd76469
Robin Painter <lochlanpainter@gmail.com>
2025-04-08 16:07:14 +1000

    Updated to 2025.2

--- example-plugin/index.js
+++ example-plugin/index.js
@@ -1,8 +1,16 @@
 const core = require("bit/core");
+const { GatewayIntentBits } = require('discord.js');
 
 module.exports = {
-    startFunction: function startFunction() {
+    start_function: function start_function() {
         // Sends a message to the Bit Core plugin and asks it to run the logging function!
         core.log(0, "Example Plugin", true, "Example Plugin for example stuffs")
+    },
+
+    define_intents: function define_intents() {
+        // Adds the MessageContent intent
+        // I recommend against adding this intent unless ABSOLUTELY required
+        // This intent allows the bot to see Message Content and is ABSOLUTELY dangerous
+        core.add_intent(GatewayIntentBits.MessageContent)
     }
 };
